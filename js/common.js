4d81f97ef9449e45530b46776251eb453bcd4ec4
Olga Vysotskaya <vysotskayavolhav@gmail.com>
2020-09-22 20:00:19 +0300

    diploma

---
+++ js/common.js
@@ -0,0 +1,23 @@
+/* var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 2,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 5000,
+    }
+}) */
+
+var swiper = new Swiper('.swiper-container', {
+        autoplay: {
+            delay: 7000,
+        }
+    }
+
+
+);
+/* $(document).ready(function() {
+    $('img').addClass('animated bounceInDown');
+}) */
